171eb482297acdb54f4d7fa2e247faf6b32e66c8
Kiyo <menager.kiyoaki@gmail.com>
2018-03-15 20:36:32 +0100

    Refactor stateless components
    using implicit returns

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-const Option = (props) => {
-  return (
-    <div>
-      <li>
-        {props.optionText}
-        <button
-          onClick={(e) => {
-            props.handleDeleteOption(props.optionText)
-          }}
+const Option = (props) => (
+  <div>
+    <li>
+      {props.optionText}
+      <button
+        onClick={(e) => {
+          props.handleDeleteOption(props.optionText)
+        }}
         >
           Remove
-        </button>
-      </li>
-    </div>
-  );
-};
+      </button>
+    </li>
+  </div>
+);
 
 export default Option;
